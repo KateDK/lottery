304d750dac578c9a4a377e6f066f86fc427cc5ce
Kate Dubitski <kate.dubitski@gmail.com>
2020-06-08 16:44:38 -0400

    Create Lottery component and render in App

---
+++ src/Lottery.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Lottery extends React.Component{
+  render(){
+    return (
+      <div>
+        <h1>Lotto</h1>
+      </div>
+    )
+  }
+}
+
+export default Lottery;
