d0ff566017db80f307006ea5380c721132515d59
Kate Dubitski <kate.dubitski@gmail.com>
2020-06-08 17:38:31 -0400

    Add ball generation method

--- src/Lottery.js
+++ src/Lottery.js
@@ -1,17 +1,35 @@
 import React from 'react';
 
 class Lottery extends React.Component{
+  state ={
+    balls: [],
+  }
+
   static defaultProps = {
     title: 'Lotto',
     numBalls:6,
     maxNum:40,
   }
 
+
+  drawBalls = (props) => {
+    console.log('yo');
+    const {numBalls,maxNum} = this.props;
+    const balls = [];
+    for(let i = 1; i <= numBalls; i++){
+      balls.push(Math.ceil(Math.random() * maxNum));
+    }
+    this.setState({balls});
+  }
+
+
   render(){
-    const {title, numBalls, maxNum} = this.props;
+    const {title} = this.props;
     return (
       <div>
         <h1>{title}</h1>
+
+        <button onClick={this.drawBalls}>generate</button>
       </div>
     )
   }
